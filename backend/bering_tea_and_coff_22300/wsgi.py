552426fd730473a8b4a07480e1649b3462d542a9
Crowdbotics <team@crowdbotics.com>
2020-11-04 17:51:17 +0000

    Update models

--- backend/bering_tea_and_coff_22300/wsgi.py
+++ backend/bering_tea_and_coff_22300/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bering_tea_and_coff_22300.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bering_tea_and_coff_22300.settings")
 
 application = get_wsgi_application()
